54db0738c4068778275b794c649e7dca008f9b7c
Le Phuong Nam <phuongnam0907@gmail.com>
2021-12-20 03:45:04 +0700

    first commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://tompotter2002:tompotter2002@cluster0.fzp1i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    secretOrKey: "secret",
+  };
