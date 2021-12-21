5e33622a90959f72df9f0eaf6998f7f4845fa6ca
Le Phuong Nam <phuongnam0907@gmail.com>
2021-12-22 06:35:12 +0700

    add port 5000

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const messages = require("./routes/api/messages");
 
 const app = express();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 const server = app.listen(port, () =>
   console.log(`Server running on port ${port}`)
